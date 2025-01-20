3d672d5321257ac8eaf03ae3ff9880fe93400ec7
vmsrdy <vmsrdy@gmail.com>
2025-01-20 21:42:07 +0530

    Add files via upload

---
+++ capexfeedback.Server/Controllers/FeedbackController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace capexfeedback.Server.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FeedbackController : ControllerBase
+    {
+
+        private static readonly string[] Summaries = new[]
+       {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly ILogger<FeedbackController> _logger;
+        public FeedbackController(ILogger<FeedbackController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "Getfeedbackdata")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
